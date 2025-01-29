152d37f030a7317ea53a48a62c962974eb7748a6
kadenboy <kadenboy0@gmail.com>
2025-01-29 10:17:16 -0600

    start button works

--- app/src/main/java/com/whitney/dontmeltthesnowman/view/MainActivity.kt
+++ app/src/main/java/com/whitney/dontmeltthesnowman/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.whitney.dontmeltthesnowman.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         setContentView(R.layout.activity_main)
+
+        //code that links start button to start the game.
+        val playButton: Button = findViewById(R.id.playButton)
+        playButton.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
