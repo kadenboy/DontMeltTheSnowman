61cc0aa6b768172dd8f3a097e0769c9ef0b1f817
kadenboy <kadenboy0@gmail.com>
2025-01-29 10:32:01 -0600

    word list is made

---
+++ app/src/main/java/com/whitney/dontmeltthesnowman/model/GameWords.kt
@@ -0,0 +1,27 @@
+package com.whitney.dontmeltthesnowman.model
+
+object GameWords {
+    val words = listOf(
+                "Apple",
+               "Bicycle",
+                "Cloud",
+                "Dragon",
+                "Elephant",
+                "Forest",
+                "Galaxy",
+                "Harmony",
+                "Iceberg",
+                "Journey",
+                "Kite",
+                "Lighthouse",
+                "Melody",
+                "Nebula",
+                "Ocean",
+                "Puzzle",
+                "Quasar",
+                "Rainbow",
+                "Symphony",
+                "Treasure"
+
+    )
+}
